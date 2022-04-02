eeadc71bf1302cace12847db76c6a398e5e8d50c
Deenaz Fathima <fathimadeenaz@gmail.com>
2022-04-02 16:57:08 +0530

    redesigned apod view

--- src/UI/ApodView.svelte
+++ src/UI/ApodView.svelte
@@ -1,24 +1,24 @@
 <script>
-    import LoadingSpinner from "./LoadingSpinner.svelte";
+	import LoadingSpinner from "./LoadingSpinner.svelte";
 
-    let imageUrl;
-    let title;
-    let description;
-    let owner;
+	let imageUrl;
+	let title;
+	let description;
+	let owner;
 
-    let fetched = false;
+	let fetched = false;
 
-    axios.get("https://astropark.herokuapp.com/apod").then((response) => {
-        // console.log(response);
-        imageUrl = response.data.url;
-        title = response.data.title;
-        description = response.data.explanation;
-        owner = response.data.copyright;
-        fetched = true;
-    });
+	axios.get("https://astropark.herokuapp.com/apod").then((response) => {
+		// console.log(response);
+		imageUrl = response.data.url;
+		title = response.data.title;
+		description = response.data.explanation;
+		owner = response.data.copyright;
+		fetched = true;
+	});
 </script>
 
-<div class="main">
+<!-- <div class="main">
     <div class="main-content">
         {#if !fetched}
             <LoadingSpinner />
@@ -31,10 +31,63 @@
             </div>
         {/if}
     </div>
-</div>
+</div> -->
+{#if !fetched}
+	<LoadingSpinner />
+{:else}
+	<div class="container">
+		<div class="apod">
+			<img src={imageUrl} alt={title} />
+			<div class="title">{title}</div>
+		</div>
+		<div class="desc">
+			<p class="info">{description}</p>
+			<p class="owner">-{owner || "Unknown"}</p>
+		</div>
+	</div>{/if}
 
 <style>
-    .main {
+	.container {
+		height: 79vh;
+		/* background-color: pink; */
+		padding: 0 20px 20px;
+		display: flex;
+	}
+
+	.apod {
+		/* background-color: blue; */
+		width: 70%;
+		/* display: flex;
+		justify-content: center; */
+		position: relative;
+	}
+
+	img {
+		width: 100%;
+		max-height: 100%;
+		object-fit: cover scale-down;
+	}
+
+	.title {
+		padding: 10px;
+		position: absolute;
+		bottom: 0;
+		right: 0;
+		text-transform: capitalize;
+		background-color: rgba(0, 0, 0, 0.5);
+		font-size: 25px;
+		font-weight: lighter;
+	}
+	.desc {
+		/* background-color: yellowgreen; */
+		padding: 0 20px;
+		width: 30%;
+	}
+
+	.info {
+		margin-block-start: 0 !important;
+	}
+	/* .main {
         position: absolute;
         top: 2.5rem;
         display: flex;
@@ -63,12 +116,12 @@
         width: 100vw;
         color: #fff;
         position: absolute;
-        /* bottom: 10px; */
+        /* bottom: 10px; 
         padding: 0 15px 15px 15px;
         background-color: rgba(0, 0, 0, 0.5);
     }
 
     .description .title {
         font-size: 30px;
-    }
+    } */
 </style>
